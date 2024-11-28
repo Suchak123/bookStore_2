1eba0dfd861aeefcc30d9283fefb3a9212154e1c
Suchak123 <niraulasuchak@gmail.com>
2024-11-28 09:11:31 +0545

    first

---
+++ config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async() => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        console.log(`Connected to MongoDb ${conn.connection.host}`)
+
+    }catch(error){
+        console.log(`Error in mongodb ${error}`.bgRed.white);
+    }
+};
+
+export default connectDB;
---
+++ models/OrderModel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+  book: {
+    type: mongoose.ObjectId,
+    ref: "Book",
+  },
+  quantity: {
+    type: Number,
+    default: 1, 
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    products: [orderItemSchema], 
+    payment: {}, 
+    buyer: {
+      type: mongoose.ObjectId,
+      ref: "User",
+    },
+    status: {
+      type: String,
+      default: "Not Processed",
+      enum: [
+        "Not Processed",
+        "Processing",
+        "Shipped",
+        "Delivered",
+        "Cancelled",
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Order", orderSchema);
